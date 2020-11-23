bacf61cdc74165e8c9c0448f41d6d53719dc63de
KoyanoBunsho <buna40950@gmail.com>
2020-11-23 09:52:23 +0900

    p que

---
+++ chap_2/use_p_que.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    priority_queue<int> pque;
+    pque.push(3);
+    pque.push(5);
+    pque.push(1);
+    while(!pque.empty()){
+        cout << pque.top() << endl;
+        pque.pop();
+    }
+    return 0;
+}
