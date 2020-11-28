857c20f89dff240d245f94570ca8416cb31c2867
KoyanoBunsho <buna40950@gmail.com>
2020-11-28 17:07:53 +0900

    kruskal

---
+++ chap_2/kruskal.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int MAX_E = 100005;
+bool comp(const edge& e1, const edge& e2){
+    return e1.cost < e2.cost;
+}
+
+edge es[MAX_E];
+int V, E;
+
+int kruskal(){
+    sort(es, es+E, comp);
+    init_union_find(V);
+    int res=0;
+    for(int i=0; i<E; i++){
+        edge e=es[i];
+        if(!same(e.u, e.v)){
+            unite(e.u, e.v);
+            res+=e.cost;
+        }
+    }
+    return res;
+}
