681ca4d57f37e184829648aeaeef50a5e4ed5ad5
KoyanoBunsho <buna40950@gmail.com>
2020-11-30 21:11:23 +0900

    kruskal

---
+++ chap_2/conscription.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int MAX_R = 100005;
+int N, M, R;
+int x[MAX_R], y[MAX_R], d[MAX_R];
+
+int main(){
+    V = N+M;
+    E=R;
+    for(int i=0; i<R; i++){
+        es[i]=(edge)(x[i], N+y[i], -d[i]);
+    }
+    cout << 10000*(N+M)+kruskal() << endl;
+    return 0;
+}
