2e2d08adcf8ab48b0edba07b96bea616bf7d5bcf
KoyanoBunsho <buna40950@gmail.com>
2020-12-15 20:37:39 +0900

    shakutori

---
+++ chap_3/Fliptile.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int MAX_M = 100005;
+const int MAX_N = 100005;
+const int dx[5]={-1, 0, 0, 0, 1};
+const int dy[5]={0, -1, 0, 1, 0};
+int M, N;
+int tile[MAX_M][MAX_N];
+int opt[MAX_M][MAX_N];
+int flip[MAX_M][MAX_N];
+
+int get(int x, int y){
+    int c=tile[x][y];
+    for(int d=0; d<5; d++){
+        
+    }
+}
+
+int main(){
+    return 0;
+}
