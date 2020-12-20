eac93f99876beec5934c8ea31df9874b5a09ad00
KoyanoBunsho <buna40950@gmail.com>
2020-12-20 17:29:33 +0900

    knapsack

---
+++ chap_3/big_knapsack.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+const int MAX_N = 10005;
+const int INF = 10005;
+int n;
+ll w[MAX_N], v[MAX_N];
+ll W;
+pair<ll, ll> ps[1<<(MAX_N/2)];
+
+int main(){
+    cin >> n;
+    for(int i=0; i<n; i++)cin >> w[i];
+    for(int i=0; i<n; i++)cin >> v[i];
+    cin >> W;
+    int n2=n/2;
+    for(int i=0; i<1<<n2; i++){
+        ll sw=0, sv=0;
+        for(int j=0; j<n2; j++){
+            if(i>>j&i){
+                sw+=w[j];
+                sv+=v[j];
+            }
+        }
+        ps[i]=make_pair(sw, sv);
+    }
+    sort(ps, ps+(1<<n2));
+    int m=1;
+    for(int i=1; i<1<<n2; i++){
+        if(ps[m-1].second<ps[i].second){
+            ps[m++]=ps[i];
+        }
+    }
+    ll res=0;
+    for(int i=0; i<1<<(n-n2); i++){
+        ll sw=0, sv=0;
+        for(int j=0; j<n-n2; j++){
+            if(i>>j&i){
+                sw+=w[n2+j];
+                sv+=v[n2+j];
+            }
+        }
+        if(sw<=W){
+            ll tv=(lower_bound(ps, ps+m, make_pair(W-sw, INF))-1)->second;
+            res=max(res, sv+tv);
+        }
+    }
+    cout << res << endl;
+}
